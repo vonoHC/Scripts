9fd4f67ea7071f8adc963ddae890275668892c90
Virgilio Herrera <virgilioherrerac@gmail.com>
2026-05-20 15:07:57 -0400

    Add timeout parameter to socket functions
    Updated socket functions to accept a timeout parameter and modified the argument parser to reflect the new default timeout value.

--- Python/Escaner/escaner.py
+++ Python/Escaner/escaner.py
@@ -1,10 +1,10 @@
 import socket as sc,argparse
 from concurrent.futures import ThreadPoolExecutor
 
-def socket_tcp(ip,port):
+def socket_tcp(ip,port,timeout):
     ip = str(sc.gethostbyname(ip))
     cliente = sc.socket(sc.AF_INET,sc.SOCK_STREAM)
-    cliente.settimeout(1)
+    cliente.settimeout(timeout)
     resultado = cliente.connect_ex((ip,port))
     cliente.close()
     if resultado == 0:
@@ -12,10 +12,10 @@ def socket_tcp(ip,port):
         return port
     return
 
-def socket_udp(ip,port):
+def socket_udp(ip,port,timeout):
     ip = str(sc.gethostbyname(ip))
     cliente = sc.socket(sc.AF_INET,sc.SOCK_DGRAM)
-    cliente.settimeout(1)
+    cliente.settimeout(timeout)
     cliente.sendto(b"hola",(ip,port))
     try:
         resultado = cliente.recvfrom(1024)
@@ -32,8 +32,8 @@ def main():
     host = parser.add_mutually_exclusive_group(required=True)
     puerto = parser.add_mutually_exclusive_group(required=True)
 
-    parser.add_argument("-t","--hilos",type=int,required=False,help="Numero de hilos. Ej. -t 100\nPor Defecto: '1000'",default=1000)
-    parser.add_argument("-to","--timeout",type=int,required=False,help="Numero de hilos. Ej. -to 1\nPor Defecto: '1'",default=0.5)
+    parser.add_argument("-t","--hilos",type=int,required=False,help="Numero de hilos. Ej. -t 100;      Por Defecto: '1000'",default=1000)
+    parser.add_argument("-to","--timeout",type=int,required=False,help="Numero de hilos. Ej. -to 1;    Por Defecto: '1'",default=1)
     parser.add_argument("-u","--udp",required=False,help="Escaneo UDP. Ej. -u ...",action="store_true")
 
     host.add_argument("-ip","--ip",type=str,help="Direccion IP del Host Objetivo. Ej. -ip 192.168.0.10")
@@ -70,13 +70,13 @@ def main():
         
     with ThreadPoolExecutor(max_workers=hilos) as ejecutor:
         if maxPort:
-            resultado = ejecutor.map(lambda p: funcion(ip or domain,p), range(1,maxPort + 1))
+            resultado = ejecutor.map(lambda p: funcion(ip or domain,p,timeout), range(1,maxPort + 1))
         elif port:
              port = [int(p) for p in port.split(",")]
-             resultado = ejecutor.map(lambda p: funcion(ip or domain,p), port)
+             resultado = ejecutor.map(lambda p: funcion(ip or domain,p,timeout), port)
         elif portRange:
             portRange = [int(p) for p in portRange.split("-")]
-            resultado = ejecutor.map(lambda p: funcion(ip or domain,p), range(portRange[0],portRange[1] + 1))
+            resultado = ejecutor.map(lambda p: funcion(ip or domain,p,timeout), range(portRange[0],portRange[1] + 1))
     
     resultado = list(resultado); resultado = [p for p in resultado if p != None]; resultado.sort()
 
